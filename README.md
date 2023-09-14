776241a98036721f8d91fdedd446f0435a7dfc78
unknown <vhisoares39@gmail.com>
2023-09-14 00:42:06 -0300

    shadcnUi Config

---
+++ README.md
@@ -0,0 +1,1 @@
+# UploadAi
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import './App.css'
+import { Button } from './components/ui/button'
 
 export function App() {
 
   return (
     <>
-      <h1>Hello World</h1>
+      <Button>test</Button>
     </>
   )
 }
